42ca7710e0457205ca50803cc3d6de7d2336512b
Eduardo Conti <es.eduardoconti@gmail.com>
2022-02-26 23:06:47 -0300

    env host api

--- src/common/Api.js
+++ src/common/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./Auth";
 
 const api = axios.create({
-  baseURL: "https://controle-financeiro-efc.herokuapp.com/",
+  baseURL: process.env.REACT_APP_API_HOST,
   headers: { "Content-Type": "application/json" },
 });
 
