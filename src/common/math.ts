faf97875d03e50198cd3e2297507cc3460c11c55
Eduardo <eduardo.conti@gazin.com.br>
2022-09-04 22:21:18 -0300

    refactor: refact median graphics

--- src/common/math.ts
+++ src/common/math.ts
@@ -2,5 +2,5 @@ export const calculateMedian = (arr: number[]): number | undefined => {
   if (!arr.length) return undefined;
   const s = [...arr].sort((a, b) => a - b);
   const mid = Math.floor(s.length / 2);
-  return s.length % 2 === 0 ? ((s[mid - 1] + s[mid]) / 2) : s[mid];
+  return Math.round((s.length % 2 === 0 ? ((s[mid - 1] + s[mid]) / 2) : s[mid]) * 100) / 100;
 };
